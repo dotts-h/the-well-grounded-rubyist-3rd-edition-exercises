19d10c2d587847b72d0550077fb86e5b9a1485cc
dotts-h <n.horia.c@hotmail.com>
2020-08-25 08:39:17 +0300

    finished ch-1-subsec-1

---
+++ chapter-1/subsec-1/c2fio.rb
@@ -0,0 +1,9 @@
+puts "Reading Celsius temperature value from data file..."
+
+celsius = File.read("temp.dat").to_i
+fahrenheit = (celsius * 9 / 5) + 32
+
+puts "Saving result to output file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
---
+++ chapter-1/subsec-1/f2cio.rb
@@ -0,0 +1,9 @@
+puts "Reading Fahrenheit temperature value from data file..."
+
+fahrenheit = File.read("temp.dat").to_i
+celsius = (fahrenheit - 32) * 5 / 9
+
+puts "Saving result to output file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts celsius
+fh.close
